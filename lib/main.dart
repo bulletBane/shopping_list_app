85ccc1d22a78338156415913e66eda8fb0bbdb1c
bulletBane <tolegena1@gmail.com>
2020-11-10 09:37:42 +0600

    feat: база данных

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './utils/dbhelper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    DbHelper helper = DbHelper();
+    helper.testDb();
+    return MaterialApp(
+        title: 'Shoppping List',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container());
+  }
+}
