85fe808ea64d7c01575f6a7561e1e29c73e7e1b2
bulletBane <tolegena1@gmail.com>
2020-11-10 10:46:55 +0600

    feat: отоборажения списка элементов для списка на странице элементов списка

--- lib/ui/items_screen.dart
+++ lib/ui/items_screen.dart
@@ -15,12 +15,43 @@ class _ItemsScreenState extends State<ItemsScreen> {
   final ShoppingList shoppingList;
   _ItemsScreenState(this.shoppingList);
 
+  DbHelper helper;
+  List<ListItem> items;
+
+  Future showData(int idList) async {
+    await helper.openDb();
+    items = await helper.getItems(idList);
+    setState(() {
+      items = items;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    helper = DbHelper();
+    showData(this.shoppingList.id);
+
     return Scaffold(
         appBar: AppBar(
           title: Text(shoppingList.name),
         ),
-        body: Container());
+        body: ListView.builder(
+            itemCount: (items != null) ? items.length : 0,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                color: Colors.white,
+                elevation: 2.0,
+                child: ListTile(
+                  title: Text(items[index].name),
+                  subtitle: Text(
+                      'Quantity: ${items[index].quantity} - Note:${items[index].note}'),
+                  onTap: () {},
+                  trailing: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {},
+                  ),
+                ),
+              );
+            }));
   }
 }
